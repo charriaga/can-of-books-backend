4607905b53786503c0ee7f64226c55d7a4bcf603
Charlie Arriaga <arriayaa@gmail.com>
2023-12-11 16:26:04 -0800

    made baby server

---
+++ src/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT !! 3000;
+const app = express();
+
+app.use(cors());
+
+app.listen(PORT, () => console.log('server is listening on port:', PORT))
