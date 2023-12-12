a11c4f8b6b18cf527b95ac3ccb1beb89679f7b73
Charlie Arriaga <arriayaa@gmail.com>
2023-12-11 19:13:50 -0800

    tidied up

--- seed.js
+++ seed.js
@@ -2,15 +2,22 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://127.0.0.1:27017/books');
 mongoose.connect(process.env.DATABASE_URL);
+console.log(process.env.DATABASE_URL);
 const Book = require('./src/Models/Book');
-
-async function seedDB() {
-    const newBook = new Book({
-        title: 'A Song of Fire and Ice',
-        description: 'Battle for the throne',
-        status: 'In stock'
-    })
-    newBook.save();
+console.log('book model', Book)
+async function seed() {
+    console.log('seeding books')
+    // const newBook = new Book({
+        try {
+            await Book.create({
+              title: 'A Song of Fire and Ice',
+              description: 'Battle for the throne',
+              status: 'In stock',
+            });
+          } catch (error) {
+            console.error('Error creating book:', error);
+          }
+          
 
     const bookTwo = new Book({
         title:'Dracula',
@@ -25,7 +32,8 @@ async function seedDB() {
         status: 'In stock',
     })
     bookThree.save();
+    console.log('finished seeding books')
     mongoose.disconnect();
 }
 
-seedDB();
+seed();
--- src/Models/Book.js
+++ src/Models/Book.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+console.log('inside book.js file')
 const { Schema } = mongoose;
 
 const bookSchema = new Schema({
