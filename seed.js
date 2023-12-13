e2545676c134573f4708a5e324ebc7d47d8c8771
Charlie Arriaga <arriayaa@gmail.com>
2023-12-12 17:45:37 -0800

    finished lab 11

--- seed.js
+++ seed.js
@@ -6,34 +6,33 @@ console.log(process.env.DATABASE_URL);
 const Book = require('./src/Models/Book');
 console.log('book model', Book)
 async function seed() {
-    console.log('seeding books')
-    // const newBook = new Book({
-        try {
-            await Book.create({
-              title: 'A Song of Fire and Ice',
-              description: 'Battle for the throne',
-              status: 'In stock',
-            });
-          } catch (error) {
-            console.error('Error creating book:', error);
-          }
-          
+  console.log('seeding books')
+  // const newBook = new Book({
+  try {
+    await Book.create({
+      title: 'A Song of Fire and Ice',
+      description: 'Battle for the throne',
+      status: 'In stock',
+    });
+    await Book.create({
+      title: 'Dracula',
+      description: 'Super old vampire invade England',
+      status: 'In stock',
+    });
 
-    const bookTwo = new Book({
-        title:'Dracula',
-        description: 'Super old vampire invade England',
-        status: 'In stock',
-    })
-    bookTwo.save();
+    await Book.create({
+      title: 'The Ice Dragon',
+      description: 'The story of a young girl, Adara, who befriends an ice dragon after the death of her mother.',
+      status: 'In stock',
+    });
+  } catch (error) {
+    console.error('Error creating book:', error);
+  }
 
-    const bookThree = new Book({
-        title: 'The Ice Dragon',
-        description: 'The story of a young girl, Adara, who befriends an ice dragon after the death of her mother.',
-        status: 'In stock',
-    })
-    bookThree.save();
-    console.log('finished seeding books')
-    mongoose.disconnect();
+
+
+  console.log('finished seeding books')
+  mongoose.disconnect();
 }
 
 seed();
